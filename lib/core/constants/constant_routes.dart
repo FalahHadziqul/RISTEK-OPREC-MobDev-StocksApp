40d0f538ea21f4fbcf60cbf389dcfcb66772d6a6
FalahHadziqul <hadziqulfalah18@gmail.com>
2026-02-23 12:44:34 +0700

    feat : native splash screen

---
+++ lib/core/constants/constant_routes.dart
@@ -0,0 +1,6 @@
+abstract class ConstantRoutes {
+  static const String splash = '/splash';
+  static const String stocks = '/';
+  static const String news = '/news';
+  static const String profile = '/profile';
+}
